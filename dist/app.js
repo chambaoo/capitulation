6c3f4b66ba8129bf18af46ff77f87619e883bd36
chambaoo <105230591+chambaoo@users.noreply.github.com>
2024-09-09 14:55:33 +0000

    add: sample html / js

--- dist/app.js
+++ dist/app.js
@@ -1,19 +1,18 @@
-class Score{}
-class Food{}
-class Foods{
-    // generics
-    elements = document.querySelectorAll<HTMLDivElement>('.food');
-
+"use strict";
+class Score {
+}
+class Food {
+}
+class Foods {
     constructor() {
+        // generics
+        this.elements = document.querySelectorAll('.food');
         this.elements.forEach(elements => {
             new Food();
-        })
+        });
     }
 }
-
 function runApp() {
     console.log('hello');
 }
-
 runApp();
-
