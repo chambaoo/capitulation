a290562c26939da4156a1f1f0c7476fbc8170f99
chambaoo <105230591+chambaoo@users.noreply.github.com>
2024-09-11 14:08:18 +0000

    add: get active elements

--- dist/food-app.js
+++ dist/food-app.js
@@ -6,6 +6,7 @@ class Food {
         this.element = element;
         this.clickEventHandler = () => {
             this.element.classList.toggle('food--active');
+            console.log(foods.activeElements);
         };
         element.addEventListener('click', this.clickEventHandler.bind(this));
     }
@@ -14,9 +15,19 @@ class Foods {
     constructor() {
         // generics
         this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
         this.elements.forEach(element => {
             new Food(element);
         });
+    }
+    get activeElements() {
+        this._activeElements = [];
+        this.elements.forEach(element => {
+            if (element.classList.contains('food--active')) {
+                this._activeElements.push(element);
+            }
+        });
+        return this._activeElements;
     }
 }
 const foods = new Foods();
--- src/food-app.ts
+++ src/food-app.ts
@@ -1,12 +1,14 @@
 class Score {}
 
 class Food {
+
     constructor(public element: HTMLElement) {
         element.addEventListener('click',this.clickEventHandler.bind(this));
     }
 
     clickEventHandler = () => {
         this.element.classList.toggle('food--active');
+        console.log(foods.activeElements);
     }
 }
 
@@ -18,6 +20,20 @@ class Foods {
         this.elements.forEach(element => {
             new Food(element);
         })
+    }
+
+    private _activeElements: HTMLDivElement[] = [];
+
+    get activeElements () {
+        this._activeElements = [];
+
+        this.elements.forEach(element => {
+            if (element.classList.contains('food--active')) {
+                this._activeElements.push(element);
+            }
+        })
+
+        return this._activeElements;
     }
 }
 
